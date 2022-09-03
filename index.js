5e40c5c02b12be3ddb8a14e654876b99fbc9f6f6
Victoria Beasley <iesnoth91@gmail.com>
2022-09-03 17:50:07 -0500

    added loop to npc walk cycle

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-// North for 1400 ms
-npc.walkNorth(1400)
-// East for 1200 ms
-.then(()=> npc.walkEast(1200))
-// South for 300 ms
-.then(()=>npc.walkSouth(300))
-// East for 1500 ms
-.then(()=> npc.walkEast(1500))
-// South for 1500 ms
-.then(()=>npc.walkSouth(1500))
-// West for 2700 ms
-.then(()=>npc.walkWest(2700))
-// North for 400 ms
-.then(()=>npc.walkNorth(400))
+function looping() {
+    // North for 1400 ms
+    npc.walkNorth(1400)
+        // East for 1200 ms
+        .then(() => npc.walkEast(1200))
+        // South for 300 ms
+        .then(() => npc.walkSouth(300))
+        // East for 1500 ms
+        .then(() => npc.walkEast(1500))
+        // South for 1500 ms
+        .then(() => npc.walkSouth(1500))
+        // West for 2700 ms
+        .then(() => npc.walkWest(2700))
+        // North for 400 ms
+        .then(() => npc.walkNorth(400))
+        //loop the process
+        .then(function(){looping()})
+}
+
+looping()
 
 // Create the inventory
 const inventory = newInventory()
